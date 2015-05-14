32b893c4502cd91de1bb6344d8a90e3c32ec51f9
Robert Feld <robertmfeld@gmail.com>
2015-05-14 23:58:29 +0000

    Stylish Hello World using bootstrap

--- picon.py
+++ picon.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	nachricht = "Hallo Welt!"
+	return render_template('index.html', nachricht=nachricht) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
